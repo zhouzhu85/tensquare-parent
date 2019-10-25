03294ce3d17cb5c6de11977be4077e8dd8490523
zhouzhu85 <734837014@qq.com>
2019-10-25 14:39:33 +0800

    短信服务模块

---
+++ tensquare-sms/src/main/java/com/tensquare/sms/SmsApplication.java
@@ -0,0 +1,16 @@
+package com.tensquare.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouzhu
+ * @Description
+ * @create 2019-10-25 9:42
+ */
+@SpringBootApplication
+public class SmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class,args);
+    }
+}
