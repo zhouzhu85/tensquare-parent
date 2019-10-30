261454e0084fa7b64f1e304c2dcbaecc3ed04a1f
zhouzhu85 <734837014@qq.com>
2019-10-30 09:12:23 +0800

    eureka服务注册模块

---
+++ tensquare-eureka/src/main/java/com/tenquare/eureka/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.tenquare.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhouzhu
+ * @Description
+ * @create 2019-10-29 17:40
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class,args);
+    }
+}
