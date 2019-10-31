d8ce624c0a51e9596dac1b45a32909edd27a2c92
zhouzhu85 <734837014@qq.com>
2019-10-31 16:45:29 +0800

    前台访问网关服务模块

---
+++ tensquare-web/src/main/java/com/tensquare/web/WebApplication.java
@@ -0,0 +1,20 @@
+package com.tensquare.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author zhouzhu
+ * @Description
+ * @create 2019-10-31 16:40
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
